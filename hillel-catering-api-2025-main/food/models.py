1fe8bbed84dd0c93583252f1474f3a7d81b7b2df
v3nux <v3nux@gmail.com>
2025-07-21 19:27:18 +0300

    f

--- hillel-catering-api-2025-main/food/models.py
+++ hillel-catering-api-2025-main/food/models.py
@@ -17,6 +17,9 @@ class Restaurant(models.Model):
 
 
 class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     class Meta:
         db_table = "dishes"
 
